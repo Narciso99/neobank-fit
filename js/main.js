c3df8f05c4fa4f1687835ac3416b44e53bd829ac
Narciso99 <126137915+Narciso99@users.noreply.github.com>
2025-09-03 18:23:07 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCwa1oA6Pwk7idNq-Uu_nNfrasXMOhCrmk",
-  authDomain: "neobank-fit.firebaseapp.com",
-  databaseURL: "https://neobank-fit-default-rtdb.firebaseio.com",
-  projectId: "neobank-fit",
-  storageBucket: "neobank-fit.firebasestorage.app",
-  messagingSenderId: "1084469171756",
-  appId: "1:1084469171756:web:72696d6690053182752389"
-};
-
-try {
-  firebase.initializeApp(firebaseConfig);
-  console.log('Firebase inicializado com sucesso');
-} catch (error) {
-  console.error('Erro ao inicializar Firebase:', error.message);
-}
-
 function showToast(message) {
   const toast = document.getElementById('toast');
   toast.textContent = message;
